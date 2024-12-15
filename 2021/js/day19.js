62b415cfc1e63d1e0595802c2ffb54efa860e757
LuosRestil <briansmithwebdeveloper@gmail.com>
2024-12-14 22:10:11 -0500

    2021 day 19 wip

--- 2021/js/day19.js
+++ 2021/js/day19.js
@@ -10,32 +10,101 @@ let scanners = getInput("day19sample.txt")
   )
   .map(scanner => {
     return {
-      scanner,
-      rotations: getRotations(scanner),
+      beacons: scanner,
+      rotations: getScannerRotations(scanner),
       normalized: false
     }
   });
 
-console.log(scanners);
-
-const queue = []; // has already normalized scanners
-
-// while (queue.length) {
-//   const curr = scanners[queue.pop()];
-//   for (let i = 0; i < scanners.length; i++) {
-//     if (finished.has(i)) continue;
-//     const other = scanners[i];
-//     const rotations = getRotations(other);
-//     const match = getMatch(curr, rotations);
-//     if (match) {
-//       scanners[i] = normalize(curr, match);
-//       finished.add(i);
-//       queue.push(i);
-//     }
-//   }
-// }
+const queue = []; // recently normalized scanners
+while (queue.length) {
+  const scanner = scanners[queue.pop()];
+  for (let i = 0; i < scanners.length; i++) {
+    const other = scanners[i];
+    if (other.normalized) continue;
+    const match = getMatch(scanner, other.rotations);
+    if (match) {
+      other.beacons = normalize(scanner, match);
+      other.normalized = true;
+      queue.push(i);
+    }
+  }
+}
 
 
-function getRotations(scanner) {
-  
+
+function getScannerRotations(scanner) {
+  const scannerRotations = [];
+  const beaconRotations = scanner.map(beacon => getBeaconRotations(beacon));
+  for (let i = 0; i < beaconRotations[0].length; i++) {
+    let rotation = [];
+    for (let j = 0; j < scanner.length; j++) {
+      rotation.push(beaconRotations[j][i]);
+    }
+    scannerRotations.push(rotation);
+  }
+}
+
+function getBeaconRotations(beacon) {
+  const rotations = [];
+  // x, y, z
+  rotations.push([beacon[0], beacon[1], beacon[2]]);
+  rotations.push([-beacon[0], beacon[1], beacon[2]]);
+  rotations.push([beacon[0], -beacon[1], beacon[2]]);
+  rotations.push([beacon[0], beacon[1], -beacon[2]]);
+  rotations.push([-beacon[0], -beacon[1], beacon[2]]);
+  rotations.push([-beacon[0], beacon[1], -beacon[2]]);
+  rotations.push([beacon[0], -beacon[1], -beacon[2]]);
+  rotations.push([-beacon[0], -beacon[1], -beacon[2]]);
+  // x, z, y
+  rotations.push([beacon[0], beacon[2], beacon[1]]);
+  rotations.push([-beacon[0], beacon[2], beacon[1]]);
+  rotations.push([beacon[0], -beacon[2], beacon[1]]);
+  rotations.push([beacon[0], beacon[2], -beacon[1]]);
+  rotations.push([-beacon[0], -beacon[2], beacon[1]]);
+  rotations.push([-beacon[0], beacon[2], -beacon[1]]);
+  rotations.push([beacon[0], -beacon[2], -beacon[1]]);
+  rotations.push([-beacon[0], -beacon[2], -beacon[1]]);
+  // y, x, z
+  rotations.push([beacon[1], beacon[0], beacon[2]]);
+  rotations.push([-beacon[1], beacon[0], beacon[2]]);
+  rotations.push([beacon[1], -beacon[0], beacon[2]]);
+  rotations.push([beacon[1], beacon[0], -beacon[2]]);
+  rotations.push([-beacon[1], -beacon[0], beacon[2]]);
+  rotations.push([-beacon[1], beacon[0], -beacon[2]]);
+  rotations.push([beacon[1], -beacon[0], -beacon[2]]);
+  rotations.push([-beacon[1], -beacon[0], -beacon[2]]);
+  // y, z, x
+  rotations.push([beacon[1], beacon[2], beacon[0]]);
+  rotations.push([-beacon[1], beacon[2], beacon[0]]);
+  rotations.push([beacon[1], -beacon[2], beacon[0]]);
+  rotations.push([beacon[1], beacon[2], -beacon[0]]);
+  rotations.push([-beacon[1], -beacon[2], beacon[0]]);
+  rotations.push([-beacon[1], beacon[2], -beacon[0]]);
+  rotations.push([beacon[1], -beacon[2], -beacon[0]]);
+  rotations.push([-beacon[1], -beacon[2], -beacon[0]]);
+  // z, x, y
+  rotations.push([beacon[2], beacon[0], beacon[1]]);
+  rotations.push([-beacon[2], beacon[0], beacon[1]]);
+  rotations.push([beacon[2], -beacon[0], beacon[1]]);
+  rotations.push([beacon[2], beacon[0], -beacon[1]]);
+  rotations.push([-beacon[2], -beacon[0], beacon[1]]);
+  rotations.push([-beacon[2], beacon[0], -beacon[1]]);
+  rotations.push([beacon[2], -beacon[0], -beacon[1]]);
+  rotations.push([-beacon[2], -beacon[0], -beacon[1]]);
+  // z, y, x
+  rotations.push([beacon[2], beacon[1], beacon[0]]);
+  rotations.push([-beacon[2], beacon[1], beacon[0]]);
+  rotations.push([beacon[2], -beacon[1], beacon[0]]);
+  rotations.push([beacon[2], beacon[1], -beacon[0]]);
+  rotations.push([-beacon[2], -beacon[1], beacon[0]]);
+  rotations.push([-beacon[2], beacon[1], -beacon[0]]);
+  rotations.push([beacon[2], -beacon[1], -beacon[0]]);
+  rotations.push([-beacon[2], -beacon[1], -beacon[0]]);
+
+  return rotations;
+}
+
+function getMatch(base, rotations) {
+
 }
