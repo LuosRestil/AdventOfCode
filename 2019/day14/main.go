e7b05d599911b515f80ffac766ab150c90626106
Brian Smith <briansmithwebdeveloper@gmail.com>
2025-05-03 22:07:13 -0400

    2019 day 14 wip

--- 2019/day14/main.go
+++ 2019/day14/main.go
@@ -51,22 +51,33 @@ func main() {
 		recipes[name] = recipe{quantity, ingredients}
 	}
 
+	var ore int64 = 0
 	inv := map[string]int{}
-	produce("FUEL", recipes, inv)
-	fmt.Printf("Part 1: %d\n", inv["ORE"])
+	produce("FUEL", recipes, inv, &ore)
+	fmt.Printf("Part 1: %d\n", ore)
+
+	ore = 0
+	inv = map[string]int{}
+	for ore < 1_000_000_000 {
+		produce("FUEL", recipes, inv, &ore)
+		fmt.Println(inv)
+		// if every element is 0 (except fuel and ore), print inv
+	}
+	
+	fmt.Printf("Part 2: %d\n", inv["FUEL"])
 }
 
-func produce(name string, recipes map[string]recipe, inv map[string]int) {
-	fmt.Printf("making %s\n", name)
+func produce(name string, recipes map[string]recipe, inv map[string]int, ore *int64) {
 	if name == "ORE" {
 		inv[name]++
+		*ore++
 	}
 	recipe := recipes[name]
 	for _, ingredient := range recipe.ingredients {
 		for inv[ingredient.name] < ingredient.quantity {
-			produce(ingredient.name, recipes, inv)
+			produce(ingredient.name, recipes, inv, ore)
 		}
 		inv[ingredient.name] -= ingredient.quantity
 	}
-
+	inv[name] += recipe.quantity
 }
