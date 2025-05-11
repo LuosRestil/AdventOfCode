a6fbb4f7089bafab25f3d88cca80b1a0f881c1b3
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-05-10 20:48:00 -0400

    2019 day 15 part 2

--- 2019/day15/main.go
+++ 2019/day15/main.go
@@ -1,8 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 	// pt 1
-	interactiveMap()
+	// interactiveMap()
 
 	// pt 2
+	file, _ := os.ReadFile("day15/map.txt")
+	lines := strings.Split(string(file), "\n")
+	grid := make([][]rune, len(lines))
+	for i, line := range lines {
+		grid[i] = []rune(line)
+	}
+	iters := 0
+	oxychar := '🟦'
+	pathchar := '🟨'
+	iterate := true
+	for iterate {
+		iterate = false
+		iters++
+		cpy := make([][]rune, len(grid))
+		for i := range grid {
+			cpy[i] = make([]rune, len(grid[i]))
+			copy(cpy[i], grid[i])
+		}
+		for i := 1; i < len(grid)-1; i++ {
+			for j := 1; j < len(grid[0])-1; j++ {
+				if grid[i][j] == pathchar {
+					iterate = true
+					if grid[i-1][j] == oxychar ||
+						grid[i+1][j] == oxychar ||
+						grid[i][j-1] == oxychar ||
+						grid[i][j+1] == oxychar {
+						cpy[i][j] = oxychar
+					}
+				}
+
+			}
+		}
+		grid = cpy
+		// displayGrid(grid)
+	}
+	fmt.Printf("Part 2: %d\n", iters-1)
 }
