6d01328888fdcedd6e1f61851b24a1328df75bd3
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-11-23 12:23:27 -0500

    2016 day19

--- 2016/days/Day19.cs
+++ 2016/days/Day19.cs
@@ -5,38 +5,20 @@ public static class Day19
     public static void Run()
     {
         int numElves = 3014387;
-        int ans = 1;
+        int ans1 = 1;
+        int ans2 = 1;
         for (int i = 4; i < numElves; i++)
         {
-            if (ans == i) ans = 1;
-            else ans += 2;
+            if (ans1 == i) ans1 = 1;
+            else ans1 += 2;
+
+            if (ans2 == i) ans2 = 1;
+            else if (ans2 < (int)Math.Ceiling(i/2.0) )
+                ans2++;
+            else
+                ans2+=2;
         }
-        Console.WriteLine($"Part 1: {ans}");
-
-        // for (int i = 5; i < 250; i++)
-        //     System.Console.WriteLine($"{i} => {Solve(i)}");
-        Console.WriteLine($"Part 2: {Solve(numElves)}");
-
-    }
-
-    private static int Solve(int numElves)
-    {
-        List<int> elves = [];
-        for (int i = 0; i < numElves; i++)
-        {
-            elves.Add(i + 1);
-        }
-
-        int elf = 0;
-        while (elves.Count > 1)
-        {
-            int target = (int)Math.Floor((elf + (elves.Count / 2.0)) % elves.Count);
-            // Console.WriteLine($"elf {elves[elf]} takes out {elves[target]}");
-            elves.RemoveAt(target);
-            elf++;
-            if (elf >= elves.Count)
-                elf = 0;
-        }
-        return elves[0];
+        Console.WriteLine($"Part 1: {ans1}");
+        Console.WriteLine($"Part2 : {ans2}");
     }
 }
