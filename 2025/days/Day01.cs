3802296ea4be628389163ca18b076091bb45874a
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-12-01 08:02:12 -0500

    2025 day 01

--- 2025/days/Day01.cs
+++ 2025/days/Day01.cs
@@ -1,15 +1,26 @@
-using System.Diagnostics;
-
 namespace _2025.days;
 
-public class Day01 
+public class Day01
 {
     public static void Run()
     {
-        long startTime = Stopwatch.GetTimestamp();
-        var lines = File.ReadLines("inputs/test.txt");
-        foreach (var line in lines)
-            Console.WriteLine(line);
-        TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+        int idx = 50;
+        int zeroes = 0;
+        int passedZeroes = 0;
+        File.ReadLines("inputs/day01.txt").ToList().ForEach(line =>
+        {
+            bool r = line.StartsWith('R');
+            int amt = int.Parse(line[1..]);
+            for (int i = 0; i < amt; i++)
+            {
+                idx += r ? 1 : -1;
+                if (idx < 0) idx = 99;
+                if (idx > 99) idx = 0;
+                if (idx == 0) passedZeroes++;
+            }
+            if (idx == 0) zeroes++;
+        });
+        Console.WriteLine($"Part 1: {zeroes}");
+        Console.WriteLine($"Part 2: {passedZeroes}");
     }
 }
