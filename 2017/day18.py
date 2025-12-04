af794713b7f93244f6740b9c8c669b26d085f1c4
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-12-03 22:31:43 -0500

    2018 day 18

---
+++ 2017/day18.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+
+
+class Instruction:
+    def __init__(self, opcode, x, y):
+        self.opcode = opcode
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"{self.opcode} {self.x} {self.y}"
+
+
+class Program:
+    i = 0
+    sent_times = 0
+    target: list[int]
+
+    def __init__(self, id, registers, queue):
+        self.id = id
+        self.registers = registers
+        self.registers['p'] = self.id
+        self.queue = queue
+
+    def emit(self, value):
+        self.target.append(value)
+
+    def tick(self, instructions):
+        jumped = False
+        instruction = instructions[self.i]
+        if instruction.opcode == 'snd':
+            x = get_value(instruction.x, self.registers)
+            self.emit(x)
+            self.sent_times += 1
+        elif instruction.opcode == 'set':
+            y = get_value(instruction.y, self.registers)
+            self.registers[instruction.x] = y
+        elif instruction.opcode == 'add':
+            y = get_value(instruction.y, self.registers)
+            self.registers[instruction.x] += y
+        elif instruction.opcode == 'mul':
+            y = get_value(instruction.y, self.registers)
+            self.registers[instruction.x] *= y
+        elif instruction.opcode == 'mod':
+            y = get_value(instruction.y, self.registers)
+            self.registers[instruction.x] %= y
+        elif instruction.opcode == 'rcv':
+            if len(self.queue) == 0:
+                return True
+            self.registers[instruction.x] = self.queue.pop(0)
+        elif instruction.opcode == 'jgz':
+            x = get_value(instruction.x, self.registers)
+            y = get_value(instruction.y, self.registers)
+            if x > 0:
+                self.i += y
+                jumped = True
+        if not jumped:
+            self.i += 1
+        return False
+
+
+def main():
+    instructions = [parse(instruction) for instruction in Path(
+        "inputs/day18.txt").read_text().splitlines()]
+    registers = {}
+    for instruction in instructions:
+        if not isinstance(instruction.x, int):
+            registers[instruction.x] = 0
+
+    p1_target = []
+    p1 = Program(0, registers.copy(), [])
+    p1.target = p1_target
+    finished = False
+    while not finished:
+        finished = p1.tick(instructions)
+    print(f"Part 1: {p1_target[-1]}")
+
+    p1 = Program(0, registers.copy(), [])
+    p2 = Program(1, registers.copy(), [])
+    p1.target = p2.queue
+    p2.target = p1.queue
+    curr = p2
+    prev_stalled = False
+    while True:
+        curr_stalled = curr.tick(instructions)
+        if curr_stalled:
+            if prev_stalled:
+                break
+            curr = p2 if curr == p1 else p1
+            prev_stalled = True
+        else:
+            prev_stalled = False
+    print(f"Part 2: {p2.sent_times}")
+
+
+def parse(instruction: str) -> Instruction:
+    split = instruction.split(' ')
+    opcode = split[0]
+    x = None
+    y = None
+    try:
+        x = int(split[1])
+    except:
+        x = split[1]
+    if len(split) == 3:
+        try:
+            y = int(split[2])
+        except:
+            y = split[2]
+    return Instruction(opcode, x, y)
+
+
+def get_value(var, registers):
+    return var if isinstance(var, int) else registers[var]
+
+
+if __name__ == "__main__":
+    main()
