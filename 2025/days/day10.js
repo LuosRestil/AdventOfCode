477e69076dcd020ae61b01646e0f0b6c3207f83c
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-12-10 16:08:14 -0500

    2025 day 10 pt1, pt2 wip

--- 2025/days/day10.js
+++ 2025/days/day10.js
@@ -1,42 +1,100 @@
 const fs = require("fs");
-const { cwd } = require("process");
+
+console.time();
 
 const lightsRe = /\[(.+)\]/;
 const buttonsRe = /\(([^\)]+)\)/g;
 const joltageRe = /\{(.+)\}/;
 
-let lines = fs.readFileSync("inputs/day10.txt", "utf-8").split("\n");
-let machines = lines.map((line) => parseMachine(line));
-let total = 0;
-for (let machine of machines) {
-  total += solve(
-    machine,
-    Array(machine.lights.length).fill("."),
-    {},
-    new Set()
-  );
+let machines = fs
+  .readFileSync("inputs/day10.txt", "utf-8")
+  .split("\n")
+  .map((line) => parseMachine(line));
+
+console.log(
+  `Part 1: ${machines.reduce((acc, curr) => (acc += bfsLights(curr)), 0)}`
+);
+
+let joltageTotal = 0;
+for (let [idx, machine] of machines.entries()) {
+  console.log("processing " + idx);
+  joltageTotal += bfsJoltage(machine);
 }
-console.log(`Part 1: ${total}`);
+console.log(`Part 2: ${joltageTotal}`);
 
-function solve(machine, lights, memo, seen) {
-  let lightsStr = lights.join("");
-  if (machine.lights === lightsStr) return 0;
-  if (memo[lightsStr]) return memo[lightsStr];
-  if (seen.has(lightsStr)) return null;
-  seen.add(lightsStr);
+console.timeEnd();
 
-  let winner = Infinity;
-  let buttonsCopy = [...machine.buttons];
-  shuffle(buttonsCopy);
-  for (let button of buttonsCopy) {
-    let newLights = [...lights];
-    for (let idx of button) newLights[idx] = newLights[idx] === "#" ? "." : "#";
-    let min = solve(machine, newLights, memo, seen);
-    if (min === null) continue;
-    if (min < winner) winner = min;
+function bfsLights(machine) {
+  let startingLights = Array(machine.lights.length).fill(".");
+  let seen = {};
+  let queue = [startingLights.join("") + ":" + 0];
+
+  while (queue.length) {
+    let state = queue.shift();
+    let sp = state.split(":");
+    let steps = parseInt(sp[1]);
+    if (sp[0] === machine.lights) {
+      return steps;
+    }
+
+    for (let button of machine.buttons) {
+      let newLights = sp[0].split("");
+      for (let idx of button) {
+        newLights[idx] = newLights[idx] === "#" ? "." : "#";
+      }
+      let newLightsStr = newLights.join("");
+      if ((seen[newLightsStr] ?? Infinity) > steps) {
+        queue.push(newLightsStr + ":" + (steps + 1));
+        seen[newLightsStr] = steps;
+      }
+    }
   }
-  memo[lightsStr] = winner + 1;
-  return winner + 1;
+}
+
+function bfsJoltage(machine) {
+  let startingJoltage = Array(machine.joltage.length).fill(0);
+  let seen = {};
+  let queue = [startingJoltage.join(",") + ":" + 0];
+
+  while (queue.length) {
+    let state = queue.shift();
+    let sp = state.split(":");
+    let joltage = sp[0].split(",").map((num) => parseInt(num));
+    let steps = parseInt(sp[1]);
+    if (arraysEqual(joltage, machine.joltage)) {
+      return steps;
+    }
+
+    for (let button of machine.buttons) {
+      let newJoltage = [...joltage];
+      for (let idx of button) {
+        newJoltage[idx]++;
+      }
+      let newJoltageStr = newJoltage.join(",");
+      if (
+        isValid(newJoltage, machine.joltage) &&
+        (seen[newJoltageStr] ?? Infinity) > steps
+      ) {
+        queue.push(newJoltageStr + ":" + (steps + 1));
+        seen[newJoltageStr] = steps;
+      }
+    }
+  }
+  return null;
+}
+
+function isValid(joltage, machineJoltage) {
+  for (let i = 0; i < joltage.length; i++)
+    if (joltage[i] > machineJoltage[i]) return false;
+  return true;
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (b[i] !== a[i]) return false;
+  }
+  return true;
 }
 
 function parseMachine(line) {
@@ -51,11 +109,4 @@ function parseMachine(line) {
     .split(",")
     .map((num) => parseInt(num));
   return { lights, buttons, joltage };
-}
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
 }
