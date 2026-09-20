6a80f2bfb788e79e2284ae468180c429a46b4649
LuosRestil <briansmithwebdeveloper@gmail.com>
2026-09-20 14:03:50 -0400

    2018 day 08

---
+++ 2018/day08.js
@@ -0,0 +1,53 @@
+import fs from "fs";
+import path from "node:path";
+
+const input = fs
+  .readFileSync(path.join(import.meta.dirname, "inputs", "day08.txt"), "utf-8")
+  .split(" ")
+  .map((num) => parseInt(num));
+
+let ptr = 0;
+let root = getNode(input, ptr);
+
+console.log(`Part 1: ${getRawMetadataValue(root)}`);
+console.log(`Part 2: ${getDerivedMetadataValue(root)}`);
+
+function getNode() {
+  let childCount = input[ptr++];
+  let metadataCount = input[ptr++];
+  let children = [];
+  let metadata = [];
+  let name = ptr;
+  for (let i = 0; i < childCount; i++) {
+    children.push(getNode());
+  }
+  for (let i = 0; i < metadataCount; i++) {
+    metadata.push(input[ptr++]);
+  }
+  return { children, metadata };
+}
+
+function getRawMetadataValue(node) {
+  let total = node.metadata.reduce((acc, curr) => acc + curr, 0);
+  for (let child of node.children) {
+    total += getRawMetadataValue(child);
+  }
+  return total;
+}
+
+function getDerivedMetadataValue(node) {
+  if (node.derivedMetadataValue === undefined) {
+    let total = 0;
+    if (!node.children.length) {
+      total = node.metadata.reduce((acc, curr) => acc + curr, 0);
+    } else {
+      for (let idx of node.metadata) {
+        if (node.children[idx-1]) {
+          total += getDerivedMetadataValue(node.children[idx-1]);
+        }
+      }
+    }
+    node.derivedMetadataValue = total;
+  }
+  return node.derivedMetadataValue;
+}
